f36788d83d550ea5882eb2d6bac5f83c2f03039d
cksgud0403 <cksgud0403@naver.com>
2024-10-01 20:01:41 +0900

    feat: 주문 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+
+    private final EntityManager em;
+
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {...}
+
+}
